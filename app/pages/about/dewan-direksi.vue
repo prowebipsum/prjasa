73356e30a8806d0e8bd93b41a9b76e1dd2465f9e
prowebipsum <prowebindo@gmail.com>
2025-08-29 14:43:19 +0700

    direksi

--- app/pages/about/dewan-direksi.vue
+++ app/pages/about/dewan-direksi.vue
@@ -44,11 +44,13 @@
             <img
               :src="selected.photo"
               alt=""
-              class="w-20 h-20 object-cover rounded-full shrink-0 border border-gray-300"
+              class="w-20 h-20 object-cover object-top rounded-full shrink-0 border border-gray-300"
             />
             <div>
               <p class="mb-0 text-2xl font-bold">{{ selected.name }}</p>
+
               <p class="text-sm mb-0">{{ selected.role }}</p>
+              <hr class="border-gray-300 my-2" />
               <div v-html="selected.bio"></div>
             </div>
           </div>
@@ -65,9 +67,9 @@ definePageMeta({
 
 const direksi = ref([
   {
-    name: "John Doe",
-    role: "CEO",
-    photo: "/img/direktur-2.jpg",
+    name: "Tiar Nabila Karbala",
+    role: "Managing Diretor",
+    photo: "/direksi/tiar-1.jpg",
     bio: `<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illo in repellat optio, autem similique sapiente a commodi sit accusamus.</p>
       <p>
       In pharetra, lacus sed condimentum bibendum, urna nibh lobortis justo, sit amet convallis elit lorem a felis. Ut eu mi sit amet lacus dignissim sollicitudin ac sed mi. Sed non eros efficitur, pharetra erat sit amet, laoreet urna. In tempor varius nunc non gravida.
