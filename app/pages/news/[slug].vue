81658d127d7644b3024b8e697305b78351f3c76f
prowebipsum <prowebindo@gmail.com>
2025-09-10 17:59:20 +0700

    news

---
+++ app/pages/news/[slug].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ route.params.slug }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute();
+</script>
+
+<style></style>
--- app/pages/news/index.vue
+++ app/pages/news/index.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10">
+      <div v-for="item in posts" class="group">
+        <nuxt-link :to="`/news/${item.slug}`">
+          <img
+            :src="item.featured_image"
+            alt=""
+            class="w-full h-56 object-cover rounded-xl mb-5 group-hover:border border-gray-200 group-hover:shadow-2xl duration-300"
+          />
+        </nuxt-link>
+        <span class="text-sm flex items-center gap-1 text-gray-500 mb-1">
+          <icon
+            name="material-symbols-light:calendar-month-outline-rounded"
+            class="text-lg"
+          />
+          {{ item.date }}
+        </span>
+        <nuxt-link :to="`/news/${item.slug}`">
+          <h5 v-html="item.title"></h5>
+        </nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+const { data: posts, status, error } = useWpPosts<PostContent>("post");
 </script>
 
-<style>
-
-</style>
+<style></style>
