909552bc1c97457ba26b035125fb74c90f72cbb1
prowebipsum <prowebindo@gmail.com>
2025-09-11 20:09:47 +0700

    blog

---
+++ app/pages/blog/[slug].vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div v-if="status === 'pending'">
+      <loading />
+    </div>
+    <article v-else class="">
+      <h1>{{ news?.title }}</h1>
+      <div class="flex gap-2 items-center justify-between mb-3">
+        <span class="text-sm flex items-center gap-1 text-gray-500 mb-1"
+          ><icon name="material-symbols-light:calendar-month-outline-rounded" />
+          {{ formatDate(news?.date) }}</span
+        >
+        <u-badge
+          v-for="item in news?.terms"
+          :key="item"
+          :label="item?.name"
+          color="neutral"
+          variant="soft"
+        >
+        </u-badge>
+      </div>
+
+      <img
+        v-if="news?.featured_image"
+        :src="news.featured_image"
+        class="mb-5 rounded-lg w-full"
+      />
+      <div v-html="news?.content" class="text-justify"></div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "single-post",
+});
+const route = useRoute();
+
+type PageContent = {
+  title: string;
+  content: string;
+  featured_image: string | null;
+  date: string;
+  tags: string[];
+};
+const { data: news, status } = useWpContent<PageContent>(route.params.slug as string);
+
+const { formatDate } = useDateFormat();
+</script>
+
+<style>
+@reference '@/assets/css/main.css';
+iframe {
+  @apply w-full;
+}
+</style>
--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
-    
+    <div v-if="status === 'pending'">
+      <loading />
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10">
+      <div v-for="item in posts" class="group">
+        <nuxt-link :to="`/news/${item.slug}`">
+          <img
+            :src="item.featured_image"
+            alt=""
+            class="w-full h-56 object-cover rounded-xl mb-5 group-hover:border border-gray-200 group-hover:shadow-2xl duration-300"
+          />
+        </nuxt-link>
+        <span class="text-xs flex items-center gap-1 text-gray-500 mb-1">
+          <icon
+            name="material-symbols-light:calendar-month-outline-rounded"
+            class="text-lg"
+          />
+          {{ formatDate(item?.date) || item.date }}
+        </span>
+        <nuxt-link :to="`/news/${item.slug}`">
+          <h5 v-html="item.title"></h5>
+        </nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+const { data: posts, status, error } = useWpPosts<PostContent>("post", {
+  taxonomy: "category",
+  term: "blog",
+});
 
+const { formatDate } = useDateFormat();
 </script>
 
-<style>
-
-</style>
+<style></style>
