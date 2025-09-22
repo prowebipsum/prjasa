5cd1dc790cecfdd2fd5c66adda4e4d7e44a0690b
prowebipsum <prowebindo@gmail.com>
2025-09-22 10:21:12 +0700

    hero

--- app/components/element/hero.vue
+++ app/components/element/hero.vue
@@ -1,13 +1,18 @@
 <template>
   <div
-    class="hero pt-20 pb-10 relative bg-cover bg-center min-h-[300px] flex flex-col justify-center"
+    class="hero pt-20 pb-10 relative bg-cover bg-center min-h-[300px] flex flex-col justify-center  overflow-hidden"
     :class="!isImage ? backgroundClass : ''"
     :style="isImage ? { backgroundImage: `url(${background})` } : {}"
   >
-    <div v-if="isImage" class="absolute inset-0 bg-white/40"></div>
+ <!--    <div v-if="isImage" class="absolute inset-0 bg-white/40"></div> -->
     <div class="container relative">
-      <h1 class="text-4xl mb-2 text-primary">{{ title }}</h1>
+      <div class="relative z-30">
+   <h1 class="text-4xl mb-2 text-primary">{{ title }}</h1>
       <p v-if="description">{{ description }}</p>
+      </div>
+   
+      <div class="max-w-[700px] w-full min-h-[300px] bg-white p-6 rounded-full absolute blur-2xl opacity-90  -bottom-10 -left-40 translate-y-1/2 ">
+      </div>
     </div>
   </div>
 </template>
