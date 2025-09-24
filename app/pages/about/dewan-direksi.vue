118e89975d1f73610ee38c9dbdc3bb75f820886c
prowebipsum <prowebindo@gmail.com>
2025-09-24 13:54:30 +0700

    Update dewan-direksi.vue

--- app/pages/about/dewan-direksi.vue
+++ app/pages/about/dewan-direksi.vue
@@ -3,32 +3,69 @@
     <div v-if="status === 'pending'">
       <loading />
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10 mb-20">
-      <div
-        v-for="item in direksi?.acf?.dewan_direksi"
-        class="p-4 rounded-xl border border-gray-300 bg-white hover:shadow duration-300"
-      >
-        <img
-          :src="item?.foto?.url"
-          :alt="item.foto?.title"
-          class="w-full lg:h-72 object-cover object-top rounded-xl mb-5"
-        />
-        <div class="flex gap-4 justify-between items-end">
-          <div>
-            <p class="mb-0">{{ item.nama }}</p>
-            <p class="text-xs mb-0">{{ item.jabatan }}</p>
-          </div>
-          <u-button
-            size="xl"
-            variant="link"
-            @click="selectDireksi(item)"
-            class="hover:scale-110 duration-200 cursor-pointer"
-          >
-            <icon name="bi:arrow-right-circle-fill" class="text-2xl text-primary" />
-          </u-button>
+   <div>
+  <!-- 3 item pertama -->
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10 mb-10">
+    <div
+      v-for="(item, i) in direksi?.acf?.dewan_direksi?.slice(0, 3)"
+      :key="i"
+      class="p-4 rounded-xl border border-gray-300 bg-white hover:shadow duration-300 relative"
+    >
+      <img
+        :src="item?.foto?.url"
+        :alt="item?.foto?.title"
+        class="w-full lg:h-72 object-cover object-top rounded-xl mb-5"
+      />
+      <div class="flex gap-4 justify-between items-end">
+        <div>
+          <p class="mb-0">{{ item.nama }}</p>
+          <p class="text-xs mb-0">{{ item.jabatan }}</p>
         </div>
+        <u-button
+          size="xl"
+          variant="link"
+          @click="selectDireksi(item)"
+          class="hover:scale-110 duration-200 cursor-pointer absolute bottom-0 right-0"
+        >
+          <icon name="bi:arrow-right-circle-fill" class="text-2xl text-primary" />
+        </u-button>
       </div>
     </div>
+  </div>
+
+  <!-- sisanya 4 kolom -->
+  <div
+    v-if="direksi?.acf?.dewan_direksi?.length > 3"
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 lg:gap-10"
+  >
+    <div
+      v-for="(item, i) in direksi?.acf?.dewan_direksi?.slice(3)"
+      :key="i"
+      class="p-4 rounded-xl border border-gray-300 bg-white hover:shadow duration-300 h-full relative"
+    >
+      <img
+        :src="item?.foto?.url"
+        :alt="item?.foto?.title"
+        class="w-full lg:h-42 object-cover object-top rounded-xl mb-5"
+      />
+      <div class="">
+        <div>
+          <p class="mb-0">{{ item.nama }}</p>
+          <p class="text-xs mb-0">{{ item.jabatan }}</p>
+        </div>
+        <u-button
+          size="xl"
+          variant="link"
+          @click="selectDireksi(item)"
+          class="hover:scale-110 duration-200 cursor-pointer absolute bottom-0 right-0"
+        >
+          <icon name="bi:arrow-right-circle-fill" class="text-2xl text-primary" />
+        </u-button>
+      </div>
+    </div>
+  </div>
+</div>
+
     <transition>
       <div
         v-if="selected"
