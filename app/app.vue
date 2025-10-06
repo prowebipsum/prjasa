60a363812bb391bbe3594dfe26d60251ad0b7225
prowebipsum <prowebindo@gmail.com>
2025-10-06 17:03:01 +0700

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,34 @@
     </nuxt-layout>
   </div>
 </template>
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-6PB5CEND33',
+      async: true,
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-6PB5CEND33', { send_page_view: false });
+      `,
+    },
+  ],
+})
+
+// tracking otomatis setiap kali pindah halaman
+onMounted(() => {
+  const router = useRouter()
+  router.afterEach((to) => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-6PB5CEND33', {
+        page_path: to.fullPath,
+      })
+    }
+  })
+})
+</script>
+
