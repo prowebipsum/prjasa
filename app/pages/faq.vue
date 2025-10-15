1186267bef9799f7a60afe2ba490b2c3d19e421e
prowebindo <prowebindo@gmail.com>
2025-10-15 14:20:48 +0700

    faq

--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -1,180 +1,134 @@
 <template>
   <div>
-        <ElementHero
-      :title="faq?.title"
-      :description="faq?.acf?.sub_title"
-      :background="faq?.featured_image"
-    />
-  
+    <ElementHero :title="faq?.title" :description="faq?.acf?.sub_title" :background="faq?.featured_image" />
+
     <!-- Loading state -->
     <div v-if="status === 'pending'">
       <loading />
     </div>
-<div class="container mt-16">
-   <div v-if="faq?.content" class="flex flex-col gap-2" v-html="faq?.content"></div>
+    <div class="container mt-16">
+      <div v-if="faq?.content" class="flex flex-col gap-2" v-html="faq?.content"></div>
 
-<div class="acc mb-10">
- <!-- Accordion -->
-    <h5 class="text-primary mb-4"> {{ $t('faq.regular') }}</h5>
-    <div
-      class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white"
-    >
-      <div v-for="(item, index) in faq?.acf?.bis_reguler" :key="index" class="accordion-item">
-        <!-- Title -->
-        <button
-          class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
-          @click="toggle(item.tanya)"
-        >
-          <span>{{ item.tanya }}</span>
-          <icon
-            name="material-symbols:expand-more-rounded"
-            class="transition-transform duration-300 text-xl"
-            :class="{ 'rotate-180': active === item.tanya }"
-          />
-        </button>
 
-        <!-- Content -->
-        <transition name="accordion">
-          <div
-            v-if="active === item.tanya"
-            class="accordion-content px-4 pb-4 text-base text-gray-700"
-            v-html="item.jawab"
-          />
-        </transition>
+      <div class="acc mb-10">
+        <!-- Accordion layanan umum-->
+        <h5 class="text-primary mb-4">{{ $t('faq.umum') }}</h5>
+        <div class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white">
+          <div v-for="(item, index) in faq?.acf?.layanan_primajasa" :key="index" class="accordion-item">
+            <!-- Title -->
+            <button
+              class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
+              @click="toggle(item.tanya)">
+              <span>{{ item.tanya }}</span>
+              <icon name="material-symbols:expand-more-rounded" class="transition-transform duration-300 text-xl"
+                :class="{ 'rotate-180': active === item.tanya }" />
+            </button>
+
+            <!-- Content -->
+            <transition name="accordion">
+              <div v-if="active === item.tanya" class="accordion-content px-4 pb-4 text-base text-gray-700"
+                v-html="item.jawab" />
+            </transition>
+          </div>
+        </div>
       </div>
-    </div>
-</div>
 
-<div class="acc mb-10">
- <!-- Accordion -->
-    <h5 class="text-primary mb-4"> {{ $t('faq.moda')}}</h5>
-    <div
-      class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white"
-    >
-      <div v-for="(item, index) in faq?.acf?.bis_moda" :key="index" class="accordion-item">
-        <!-- Title -->
-        <button
-          class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
-          @click="toggle(item.tanya)"
-        >
-          <span>{{ item.tanya }}</span>
-          <icon
-            name="material-symbols:expand-more-rounded"
-            class="transition-transform duration-300 text-xl"
-            :class="{ 'rotate-180': active === item.tanya }"
-          />
-        </button>
+      <div class="acc mb-10">
+        <!-- Accordion -->
+        <h5 class="text-primary mb-4"> {{ $t('faq.regular') }}</h5>
+        <div class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white">
+          <div v-for="(item, index) in faq?.acf?.bis_reguler" :key="index" class="accordion-item">
+            <!-- Title -->
+            <button
+              class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
+              @click="toggle(item.tanya)">
+              <span>{{ item.tanya }}</span>
+              <icon name="material-symbols:expand-more-rounded" class="transition-transform duration-300 text-xl"
+                :class="{ 'rotate-180': active === item.tanya }" />
+            </button>
 
-        <!-- Content -->
-        <transition name="accordion">
-          <div
-            v-if="active === item.tanya"
-            class="accordion-content px-4 pb-4 text-base text-gray-700"
-            v-html="item.jawab"
-          />
-        </transition>
+            <!-- Content -->
+            <transition name="accordion">
+              <div v-if="active === item.tanya" class="accordion-content px-4 pb-4 text-base text-gray-700"
+                v-html="item.jawab" />
+            </transition>
+          </div>
+        </div>
       </div>
-    </div>
-</div>
-   
-   <div class="acc mb-10">
- <!-- Accordion -->
-    <h5 class="text-primary mb-4"> {{ $t('faq.shuttle') }}</h5>
-    <div
-      class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white"
-    >
-      <div v-for="(item, index) in faq?.acf?.shuttle" :key="index" class="accordion-item">
-        <!-- Title -->
-        <button
-          class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
-          @click="toggle(item.tanya)"
-        >
-          <span>{{ item.tanya }}</span>
-          <icon
-            name="material-symbols:expand-more-rounded"
-            class="transition-transform duration-300 text-xl"
-            :class="{ 'rotate-180': active === item.tanya }"
-          />
-        </button>
 
-        <!-- Content -->
-        <transition name="accordion">
-          <div
-            v-if="active === item.tanya"
-            class="accordion-content px-4 pb-4 text-base text-gray-700"
-            v-html="item.jawab"
-          />
-        </transition>
+      <div class="acc mb-10">
+        <!-- Accordion -->
+        <h5 class="text-primary mb-4"> {{ $t('faq.moda') }}</h5>
+        <div class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white">
+          <div v-for="(item, index) in faq?.acf?.bis_moda" :key="index" class="accordion-item">
+            <!-- Title -->
+            <button
+              class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
+              @click="toggle(item.tanya)">
+              <span>{{ item.tanya }}</span>
+              <icon name="material-symbols:expand-more-rounded" class="transition-transform duration-300 text-xl"
+                :class="{ 'rotate-180': active === item.tanya }" />
+            </button>
+
+            <!-- Content -->
+            <transition name="accordion">
+              <div v-if="active === item.tanya" class="accordion-content px-4 pb-4 text-base text-gray-700"
+                v-html="item.jawab" />
+            </transition>
+          </div>
+        </div>
       </div>
-    </div>
-</div>
 
- <div class="acc mb-10">
- <!-- Accordion -->
-    <h5 class="text-primary mb-4">{{ $t('faq.pariwisata')}}</h5>
-    <div
-      class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white"
-    >
-      <div v-for="(item, index) in faq?.acf?.bis_pariwisata" :key="index" class="accordion-item">
-        <!-- Title -->
-        <button
-          class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
-          @click="toggle(item.tanya)"
-        >
-          <span>{{ item.tanya }}</span>
-          <icon
-            name="material-symbols:expand-more-rounded"
-            class="transition-transform duration-300 text-xl"
-            :class="{ 'rotate-180': active === item.tanya }"
-          />
-        </button>
+      <div class="acc mb-10">
+        <!-- Accordion -->
+        <h5 class="text-primary mb-4"> {{ $t('faq.shuttle') }}</h5>
+        <div class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white">
+          <div v-for="(item, index) in faq?.acf?.shuttle" :key="index" class="accordion-item">
+            <!-- Title -->
+            <button
+              class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
+              @click="toggle(item.tanya)">
+              <span>{{ item.tanya }}</span>
+              <icon name="material-symbols:expand-more-rounded" class="transition-transform duration-300 text-xl"
+                :class="{ 'rotate-180': active === item.tanya }" />
+            </button>
 
-        <!-- Content -->
-        <transition name="accordion">
-          <div
-            v-if="active === item.tanya"
-            class="accordion-content px-4 pb-4 text-base text-gray-700"
-            v-html="item.jawab"
-          />
-        </transition>
+            <!-- Content -->
+            <transition name="accordion">
+              <div v-if="active === item.tanya" class="accordion-content px-4 pb-4 text-base text-gray-700"
+                v-html="item.jawab" />
+            </transition>
+          </div>
+        </div>
       </div>
-    </div>
-</div>
 
- <div class="acc mb-10">
- <!-- Accordion layanan umum-->
-    <h5 class="text-primary mb-4">{{ $t('faq.umum')}}</h5>
-    <div
-      class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white"
-    >
-      <div v-for="(item, index) in faq?.acf?.layanan_primajasa" :key="index" class="accordion-item">
-        <!-- Title -->
-        <button
-          class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
-          @click="toggle(item.tanya)"
-        >
-          <span>{{ item.tanya }}</span>
-          <icon
-            name="material-symbols:expand-more-rounded"
-            class="transition-transform duration-300 text-xl"
-            :class="{ 'rotate-180': active === item.tanya }"
-          />
-        </button>
+      <div class="acc mb-10">
+        <!-- Accordion -->
+        <h5 class="text-primary mb-4">{{ $t('faq.pariwisata') }}</h5>
+        <div class="accordion divide-y divide-gray-200 border border-gray-300 rounded-xl bg-white">
+          <div v-for="(item, index) in faq?.acf?.bis_pariwisata" :key="index" class="accordion-item">
+            <!-- Title -->
+            <button
+              class="accordion-title flex w-full  justify-between px-4 py-3 text-left text-lg font-semibold cursor-pointer"
+              @click="toggle(item.tanya)">
+              <span>{{ item.tanya }}</span>
+              <icon name="material-symbols:expand-more-rounded" class="transition-transform duration-300 text-xl"
+                :class="{ 'rotate-180': active === item.tanya }" />
+            </button>
 
-        <!-- Content -->
-        <transition name="accordion">
-          <div
-            v-if="active === item.tanya"
-            class="accordion-content px-4 pb-4 text-base text-gray-700"
-            v-html="item.jawab"
-          />
-        </transition>
+            <!-- Content -->
+            <transition name="accordion">
+              <div v-if="active === item.tanya" class="accordion-content px-4 pb-4 text-base text-gray-700"
+                v-html="item.jawab" />
+            </transition>
+          </div>
+        </div>
       </div>
+
+
     </div>
-</div>
-</div>
     <!-- Konten statis sebelum FAQ -->
-   
+
   </div>
 </template>
 
